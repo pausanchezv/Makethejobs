19e6ece428305b126d064f2ab53726c67d7aef2a
pausanchezv <pausanchez.admifin@gmail.com>
2018-12-16 07:09:50 +0100

    MK-001 Accounts

--- website/makethejobs/accounts/admin.py
+++ website/makethejobs/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+
+admin.site.register(User, UserAdmin)
